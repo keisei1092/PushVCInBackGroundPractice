279977157fc801f8a2fe0b2f581ad8e770952362
keisei_1092 <keiseis1092@gmail.com>
2017-07-14 14:39:35 +0900

    Initial Commit

---
+++ PushVCInBackGroundPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PushVCInBackGroundPractice
+//
+//  Created by Keisei Saito on 2017/07/14.
+//  Copyright © 2017 Keisei Saito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
